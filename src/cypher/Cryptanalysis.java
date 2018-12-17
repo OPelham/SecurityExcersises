8d3413cb2231f977718b322a0daad47490067f26
OPelham <oliverpelham@gmail.com>
2018-12-18 10:47:48 +1300

    Testing Ali's example code

--- src/cypher/Cryptanalysis.java
+++ src/cypher/Cryptanalysis.java
@@ -20,11 +20,11 @@ public class Cryptanalysis {
 	public static int bruteForce(String cryptoText){
 		ArrayList<String> attempts = new ArrayList<>();
 		ArrayList<Integer> keys = new ArrayList<>();
-		
+
 		for(int i=0; i<Cypher.alphabet.length() ; i++) {
 			String attempt = Cypher.decrypt(cryptoText, i);
-			
-			
+
+
 			//if over threashold then add to keys
 			System.out.println(CheckAPI(attempt));
 			if(CheckAPI(attempt)> 0.5) {
@@ -32,30 +32,24 @@ public class Cryptanalysis {
 				keys.add(i);
 				return i;
 			}
-			
+
 			//insert test here if over threshhold save
-			
+
 		}
-		
-		//api check
-		
-		
-		//manual check
-//		for (int i=0; i<attempts.size(); i++) {
-//			System.out.println("key: " + keys.get(i) + "| decryption: " + attempts.get(i));
-//		}
-		
+
+
 		return -1;
-		
+
 	}
 	//to check can see how many words are in dictionary
-	
-	
+
+
 	//check frequencies of characters and compare to expected frequencies. 
 	public static void frequencyAnalysis(String cryptoText) {
 		//1. make a map of each alphabet char and its freq
 		//2. one with most freq try shift to 'e' or space, if not then try next one?
 		ArrayList<Integer> frequecyList = new ArrayList<>();
+		ArrayList<Integer> orderedPos = new ArrayList<>();
 		//init freqlist size
 		for (int i=0; i<Cypher.alphabet.length(); i++) {
 			frequecyList.add(0);
@@ -66,17 +60,30 @@ public class Cryptanalysis {
 			if(cryptoText.charAt(i) == Cypher.alphabet.charAt(i)) {
 				frequecyList.set(i, frequecyList.get(i) + 1);
 			}
-			
+
 		}
 		//make copy and rank then find these values within frequencyList?
+
+		int maxPos = -1;
+		for (int j=0; j<20; j++) { //try 20 most common chars
+			for (int i=0; i<frequecyList.size(); i++) {
+				if(frequecyList.get(i) > maxPos && (!(orderedPos.contains(i)))) {
+					maxPos = i;
+				}
+			}
+			orderedPos.add(maxPos);
+		}
 		
+		//now try first most common as a and test if not try next and test etc
+		//calculate shift from most frequent pos to pos of e
 		
-	
+
+
 	}
-	
-	
-	
-	
+
+
+
+
 	public static double CheckAPI(String attempt) {
 		//break down to words in an array, pass each into api and collect double of percent correct
 		String[] words = attempt.split(" ");
@@ -85,37 +92,37 @@ public class Cryptanalysis {
 			if(checkWord(word) == true) {
 				matchCount ++;
 			}
-			
+
 		}
-		
-		
+
+
 		return matchCount/((double)(words.length));
 	}
-	
+
 	public static boolean checkWord(String word)
-    {
-         word = word.toLowerCase();
-                
-         String app_id="7885cef5";
-         String app_key="cd5681738ddefffce6127b3ff26802b7";
-         try
-         {
-            URL url = new  URL("https://od-api.oxforddictionaries.com:443/api/v1/entries/en" + "/" + word);       
-            HttpsURLConnection urlConnection=(HttpsURLConnection) url.openConnection();            
-            urlConnection.setRequestProperty("Accept", "application/json");
-            urlConnection.setRequestProperty("app_id", app_id);
-            urlConnection.setRequestProperty("app_key", app_key);      
-            // read the output from the server
-            BufferedReader reader=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            String line=null;
-            if ((line=reader.readLine()) !=null)
-                return true;
-        }
-        catch (Exception e)
-        {
-            //            Do nothing
-        }
-        return false;
-    }
-	
+	{
+		word = word.toLowerCase();
+
+		String app_id="7885cef5";
+		String app_key="cd5681738ddefffce6127b3ff26802b7";
+		try
+		{
+			URL url = new  URL("https://od-api.oxforddictionaries.com:443/api/v1/entries/en" + "/" + word);       
+			HttpsURLConnection urlConnection=(HttpsURLConnection) url.openConnection();            
+			urlConnection.setRequestProperty("Accept", "application/json");
+			urlConnection.setRequestProperty("app_id", app_id);
+			urlConnection.setRequestProperty("app_key", app_key);      
+			// read the output from the server
+			BufferedReader reader=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+			String line=null;
+			if ((line=reader.readLine()) !=null)
+				return true;
+		}
+		catch (Exception e)
+		{
+			//            Do nothing
+		}
+		return false;
+	}
+
 }
