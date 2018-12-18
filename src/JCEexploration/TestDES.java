49198e22fed87aa4b3cccd3b2aa776d6e2c9fa65
OPelham <oliverpelham@gmail.com>
2018-12-18 14:25:45 +1300

    Exploring more of Ali's example code

---
+++ src/JCEexploration/TestDES.java
@@ -0,0 +1,54 @@
+package JCEexploration;
+
+//import com.sun.xml.internal.messaging.saaj.util.Base64;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.Security;
+import java.util.Iterator;
+import javax.crypto.Cipher;
+import sun.misc.BASE64Encoder;
+
+/**
+*
+* @author ahmed
+*/
+public class TestDES {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args){
+      // TODO code application logic here
+      try
+      {
+          DES des1 = new DES();
+          String msg = "Welcome everybody. This is Ali :)";
+          
+          
+          System.out.println("The plain text: "+msg);
+          
+          byte[] encText = des1.encrypt(msg);
+          
+          //String cipherText = new String(encText);
+          //System.out.println("The DES encrypted message: "+ cipherText);
+          System.out.println("The DES encrypted message 64: "+ (new BASE64Encoder().encode(encText)));
+          
+          des1.saveKey("sessionKey1.key");
+                      
+          DES des2 = new DES();
+          des2.loadKey("sessionKey1.key");
+          //des2.setSecretkey(des1.getSecretkey());      
+       
+          
+          String decText = des2.decrypt(encText);
+          System.out.println("The DES decrypted message: "+decText);
+          
+      }
+      catch(Exception e)
+      {
+          System.out.println("Error in DES: "+e);   
+          e.printStackTrace();
+      }
+  }
+  
+}
--- src/JCEexploration/TestDESSimple.java
+++ src/JCEexploration/TestDESSimple.java
@@ -1,5 +1,6 @@
 package JCEexploration;
 
+import java.math.BigInteger;
 //import com.sun.xml.internal.messaging.saaj.util.Base64;
 import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
@@ -13,11 +14,17 @@ import sun.misc.BASE64Encoder;
 * @author ahmed
 */
 public class TestDESSimple {
-
+//static BigInteger bi1 = new BigInteger("89123450892345708193456081345608123456087134560871356");
+//static BigInteger bi2 = new BigInteger("098096358098098345609809825460980542680980254860980825460980982546098098245609");
+//static BigInteger bi3 = new BigInteger("0");
   /**
    * @param args the command line arguments
    */
   public static void main(String[] args){
+//	  bi3 = bi3.add(bi1);
+//	  bi3 = bi3.add(bi1);
+//	  System.out.println(bi3);
+	  
       // TODO code application logic here
       try
       {
@@ -26,7 +33,7 @@ public class TestDESSimple {
           
           System.out.println("The plain text: "+msg);            
           byte[] encText = des1.encrypt(msg);       
-          System.out.println(new String(encText));
+//          System.out.println("output not in 64" + new String(encText));
           System.out.println("The DES encrypted message 64: "+ (new BASE64Encoder().encode(encText)));   //to 64 bit for campatability sending across netwrok and across diff sustems...          
           String decText = des1.decrypt(encText);
           System.out.println("The DES decrypted message: "+decText);
